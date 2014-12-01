bb3ee1f15d2647a7f6602f9cc4620aa9476e0c68
Tomohiro Oda <tomohiro.tomo.oda@gmail.com>
2014-12-01 20:50:32 +0900

    EnergyTreeView: equal arrangement per EQ/TS/DC

--- src/net/sourceforge/rmapviewer/rmapjmol/EnergyTreeView.java
+++ src/net/sourceforge/rmapviewer/rmapjmol/EnergyTreeView.java
@@ -12,16 +12,29 @@ import java.util.ArrayList;
  */
 public class EnergyTreeView extends JPanel implements ActionListener {
     private double[] energies;
+    private int[] captionIndices;
     private double min_energy;
     private double max_energy;
     private RMapJmol jmol;
     private Timer timer = new Timer(100, this);
 
+    private double framePosition(int index) {
+        int i;
+        for (i = 0; i < captionIndices.length - 1; i++) {
+            if (captionIndices[i+1] >= index)
+                break;
+        }
+        if (i >= captionIndices.length - 1)
+            return (double)(captionIndices.length - 1);
+        return (double)i + (double)(index - captionIndices[i]) / (double)(captionIndices[i+1]-captionIndices[i]);
+    }
+
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
+        FontMetrics fontMetrics = graphics.getFontMetrics();
         int frame = jmol.getFrame();
-        double scale_x = (double)(getWidth() - 60) / (energies.length+1);
+        double scale_x = (double)(getWidth() - 60) / (captionIndices.length);
         double scale_y = (double)(getHeight() - 80) / (max_energy - min_energy);
         int offset_x = 50;
         int offset_y = 40;
@@ -29,36 +42,39 @@ public class EnergyTreeView extends JPanel implements ActionListener {
         for (int i = 1; i < energies.length; i++) {
             double e1 = max_energy - energies[i-1];
             double e2 = max_energy - energies[i];
-            int x1 = (int) (i * scale_x) + offset_x;
+            int x1 = (int) (framePosition(i) * scale_x) + offset_x;
             int y1 = (int) (e1 * scale_y) + offset_y;
-            int x2 = (int) ((i+1) * scale_x) + offset_x;
+            int x2 = (int) (framePosition(i+1) * scale_x) + offset_x;
             int y2 = (int) (e2 * scale_y) + offset_y;
             graphics.setColor(Color.BLACK);
             graphics.drawLine(x1, y1, x2, y2);
         }
         ((Graphics2D)graphics).setStroke(new BasicStroke(1));
-        int base_x = (int) scale_x + offset_x;
+        int base_x = (int) offset_x;
         int base_y = (int)((max_energy - min_energy) * scale_y) + offset_y;
+        double magnitude = Math.pow(10.0, Math.floor(Math.log10(max_energy - min_energy)));
+        graphics.setColor(Color.BLACK);
+        graphics.drawString("kJ/mol", 10, 30);
+        for (double e = 0.0; e <= max_energy-min_energy; e += magnitude) {
+            int y = (int)((max_energy - e - min_energy) * scale_y) + offset_y;
+            graphics.drawLine(base_x, y, getWidth()-10, y);
+            String eStr = ""+e;
+            graphics.drawString(eStr, base_x - fontMetrics.stringWidth(eStr) - 2, y + fontMetrics.getAscent()/2);
+        }
+        for (int i = 0; i < captionIndices.length; i++) {
+            int x = (int)(i*scale_x + offset_x);
+            String caption = jmol.getCaption(captionIndices[i]);
+            graphics.setColor(Color.BLACK);
+            graphics.drawString(caption, x-fontMetrics.stringWidth(caption)/2, base_y + fontMetrics.getHeight()+2);
+            graphics.setColor(Color.LIGHT_GRAY);
+            graphics.drawLine(x, offset_y, x, base_y);
+        }
+        /* shall draw a grid on Y axis */
         graphics.setColor(Color.BLACK);
         graphics.drawLine(base_x, offset_y, base_x, base_y);
         graphics.drawLine(base_x, (int)(max_energy*scale_y)+offset_y, getWidth()-10, (int)(max_energy*scale_y)+offset_y);
         graphics.setColor(Color.RED);
-        graphics.drawLine((int)(frame*scale_x)+offset_x, 0, (int)(frame*scale_x)+offset_x, getHeight()-18);
-        FontMetrics fontMetrics = graphics.getFontMetrics();
-        graphics.setColor(Color.BLACK);
-        for (int i = 1; i <= energies.length; i++) {
-            String label = jmol.getCaption(i);
-            if (label.length() > 0) {
-                int width = fontMetrics.stringWidth(label);
-                graphics.drawString(label, (int)(i*scale_x)+offset_x-width/2, getHeight()-5);
-                double e = energies[i-1];
-                int e10 = (int)(e * 2625.49962);
-                String eStr = ""+e10/10+"."+e10%10;
-                int w = fontMetrics.stringWidth(eStr);
-                graphics.drawString(eStr, 40-w, (int)((max_energy-e)*scale_y)+offset_y+fontMetrics.getAscent()/2);
-            }
-        }
-
+        graphics.drawLine((int)(framePosition(frame)*scale_x)+offset_x, 0, (int)(framePosition(frame)*scale_x)+offset_x, getHeight()-18);
     }
 
     public EnergyTreeView(RMapJmol jmol, double[] energies) {
@@ -73,6 +89,17 @@ public class EnergyTreeView extends JPanel implements ActionListener {
                 this.min_energy = e;
             if (e > this.max_energy)
                 this.max_energy = e;
+        }
+        int number_of_captions = 0;
+        for (int i = 0; i < energies.length; i++) {
+            if (this.jmol.getCaption(i).length() > 0)
+                ++number_of_captions;
+        }
+        this.captionIndices = new int[number_of_captions];
+        number_of_captions = 0;
+        for (int i = 0; i < energies.length; i++) {
+            if (this.jmol.getCaption(i).length() > 0)
+                this.captionIndices[number_of_captions++] = i;
         }
         timer.start();
     }
@@ -92,7 +119,7 @@ public class EnergyTreeView extends JPanel implements ActionListener {
         double[] energies = new double[lines.size()];
         double base_energy = (double)Double.parseDouble(lines.get(0));
         for (int i = 0; i < energies.length; i++) {
-            energies[i] = (double)Double.parseDouble(lines.get(i)) - base_energy;
+            energies[i] = ((double)Double.parseDouble(lines.get(i)) - base_energy) * 262.549962;
         }
         return energies;
     }
